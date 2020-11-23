8f4087e1d8061031305224592f1e2ab1fe093f62
ray <shiyan@eusunpower.com>
2020-11-24 00:19:16 +0800

    增加引导页面

---
+++ app/src/main/java/com/justforfun/calc/guide/GuideActivity.kt
@@ -0,0 +1,132 @@
+package com.justforfun.calc.guide
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.justforfun.calc.R
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class GuideActivity : AppCompatActivity() {
+    private lateinit var viewPager: ViewPager2
+    private val hideHandler = Handler()
+
+    @SuppressLint("InlinedApi")
+    private val hidePart2Runnable = Runnable {
+        // Delayed removal of status and navigation bar
+
+        // Note that some of these constants are new as of API 16 (Jelly Bean)
+        // and API 19 (KitKat). It is safe to use them, as they are inlined
+        // at compile-time and do nothing on earlier devices.
+        viewPager.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
+    private val showPart2Runnable = Runnable {
+        // Delayed display of UI elements
+        supportActionBar?.show()
+//        fullscreenContentControls.visibility = View.VISIBLE
+    }
+    private var isFullscreen: Boolean = false
+
+    private val hideRunnable = Runnable { hide() }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_guide)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        isFullscreen = true
+
+        viewPager = findViewById(R.id.vp)
+        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        val pagerAdapter = GuideViewPagerAdapter()
+        with(viewPager) {
+            adapter = pagerAdapter
+//            isUserInputEnabled = false
+            orientation = ViewPager2.ORIENTATION_HORIZONTAL
+//            setCurrentItem(0, true)
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {})
+        }
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(10)
+    }
+
+    private fun toggle() {
+        if (isFullscreen) {
+            hide()
+        } else {
+            show()
+        }
+    }
+
+    private fun hide() {
+        // Hide UI first
+        supportActionBar?.hide()
+//        fullscreenContentControls.visibility = View.GONE
+        isFullscreen = false
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        hideHandler.removeCallbacks(showPart2Runnable)
+        hideHandler.postDelayed(hidePart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    private fun show() {
+        // Show the system bar
+        viewPager.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        isFullscreen = true
+
+        // Schedule a runnable to display UI elements after a delay
+        hideHandler.removeCallbacks(hidePart2Runnable)
+        hideHandler.postDelayed(showPart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    /**
+     * Schedules a call to hide() in [delayMillis], canceling any
+     * previously scheduled calls.
+     */
+    private fun delayedHide(delayMillis: Int) {
+        hideHandler.removeCallbacks(hideRunnable)
+        hideHandler.postDelayed(hideRunnable, delayMillis.toLong())
+    }
+
+    companion object {
+        /**
+         * Whether or not the system UI should be auto-hidden after
+         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
+         */
+        private const val AUTO_HIDE = true
+
+        /**
+         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
+         * user interaction before hiding the system UI.
+         */
+        private const val AUTO_HIDE_DELAY_MILLIS = 3000
+
+        /**
+         * Some older devices needs a small delay between UI widget updates
+         * and a change of the status and navigation bar.
+         */
+        private const val UI_ANIMATION_DELAY = 300
+    }
+}
---
+++ app/src/main/java/com/justforfun/calc/guide/GuideViewPagerAdapter.kt
@@ -0,0 +1,54 @@
+package com.justforfun.calc.guide
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.justforfun.calc.R
+
+class GuideViewPagerAdapter : RecyclerView.Adapter<GuideViewPagerAdapter.ViewHolder>() {
+
+    val imgs = listOf(R.mipmap.guide_img_1, R.mipmap.guide_img_2, R.mipmap.guide_img_3)
+    val listTip1 = listOf("计算", "随时记录", "我是一款支持复数运算和解方程的综合科学计")
+    val listTip2 = listOf("是一种别具匠心的艺术", "好记性不如烂笔头", "")
+    val tip1Size = listOf(30f, 30f, 20f)
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tip1: TextView
+        var tip2: TextView
+        var img: ImageView
+        var btn: Button
+        var tvBottom: TextView
+
+        init {
+            tip1 = itemView.findViewById(R.id.tv_tip1)
+            tip2 = itemView.findViewById(R.id.tv_tip2)
+            img = itemView.findViewById(R.id.iv_tip)
+            btn = itemView.findViewById(R.id.btn_go)
+            tvBottom = itemView.findViewById(R.id.tv_bottom)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.guide_viewpager_item, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tip1.setText(listTip1[position])
+        holder.tip1.textSize = tip1Size[position]
+        holder.tip2.setText(listTip2[position])
+        holder.tip2.visibility = if (position == 2) View.GONE else View.VISIBLE
+        holder.img.setImageResource(imgs[position])
+        holder.btn.visibility = if (position != 2) View.GONE else View.VISIBLE
+        holder.tvBottom.visibility = if (position != 2) View.GONE else View.VISIBLE
+    }
+
+    override fun getItemCount(): Int = 3
+
+}
+
