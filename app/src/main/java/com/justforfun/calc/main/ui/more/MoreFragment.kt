07d0dc619f57799dd9a36a07efddcfcadf3f977b
shiyan <shiyan@eusunpower.co>
2020-11-27 21:32:00 +0800

    检测更新

--- app/src/main/java/com/justforfun/calc/main/ui/more/MoreFragment.kt
+++ app/src/main/java/com/justforfun/calc/main/ui/more/MoreFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -92,7 +93,7 @@ class MoreFragment : Fragment() {
                         }
                     })
                 } else {
-//                    Toast.makeText(context, "已是最新版本", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, "已是最新版本", Toast.LENGTH_SHORT).show();
                 }
             }
 
