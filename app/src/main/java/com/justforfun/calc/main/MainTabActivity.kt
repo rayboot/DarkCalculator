9ad2dce6433446d267ff2321799cd9a0fefcbd3a
ray <shiyan@eusunpower.com>
2020-11-29 19:33:11 +0800

    串页面

--- app/src/main/java/com/justforfun/calc/main/MainTabActivity.kt
+++ app/src/main/java/com/justforfun/calc/main/MainTabActivity.kt
@@ -3,6 +3,7 @@ package com.justforfun.calc
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,6 +15,10 @@ class MainTabActivity : BaseActivity() {
             context.startActivity(intent)
         }
     }
+
+
+    private var mBackPressed: Long = 0
+    private val TIME_EXIT = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,5 +33,15 @@ class MainTabActivity : BaseActivity() {
 //                R.id.navigation_calc, R.id.navigation_memo, R.id.navigation_more))
 //        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    //    private MenuItem godMenuItem;
+    override fun onBackPressed() {
+        if (mBackPressed + TIME_EXIT > System.currentTimeMillis()) {
+            super.onBackPressed()
+        } else {
+            Toast.makeText(this, "再点击一次返回退出程序", Toast.LENGTH_SHORT).show()
+            mBackPressed = System.currentTimeMillis();
+        }
     }
 }
